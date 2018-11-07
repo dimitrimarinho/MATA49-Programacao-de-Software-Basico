51f90dc3c06399518b49d551fda4902ad4978290
Dimitri Marinho <dimitri.santana.marinho@gmail.com>
2018-11-07 20:51:43 -0300

    Exercise 06 completed! Uploaded on Moodle.

--- Exercicios/Exercicio 06/exercise_06.asm
+++ Exercicios/Exercicio 06/exercise_06.asm
@@ -1,89 +1,182 @@
-; nasm -f elf exercise_06.asm && ld -m elf_i386 -s -o exercise_06 exercise_06.o
+; Diretriz de compilação do código: nasm -f elf exercise_06.asm && ld -m elf_i386 -s -o exercise_06 exercise_06.o && ./exercise_06
 segment .data
 
-segment .bss ; leitura dos caracteres dos números
+segment .bss
 
   vetor1 resb 3
   vetor2 resb 3
   enter1 resb 1
-  divisor resd 1
-  number1 resb 1
-  number2 resb 1
-  resultado resd 1
-  vetor_print resb 4
+  number1 resw 1
+  number2 resw 1
+  resultado resw 1
+  vetor_print3 resw 3
+  vetor_print4 resw 4
 
 segment .text
 
-  global _start
+    global _start
 
 _start:
 
-  mov EAX, 3      ; 3 indica ao sistema para executar a leitura
-  mov EBX, 0      ; lê da entrada padrão
-  mov ECX, vetor1   ; quem deve receber o caractere lido
-  mov EDX, 3        ; tamanho da entrada
-  int 0x80          ; chamada do kernel do linux
+  le_vetor1:        ; Este código lê os primeiros chars e armazena o primeiro número de três dígitos no vetor1
+    mov EAX, 3      ; 3 indica ao sistema para executar a leitura
+    mov EBX, 0      ; lê da entrada padrão
+    mov ECX, vetor1   ; quem deve receber o caractere lido
+    mov EDX, 3        ; tamanho da entrada
+    int 0x80          ; chamada do kernel do linux
 
-  ; esse código aqui é para ler o \n da entrada do primeiro numero
-  mov EAX, 3      ; 3 indica ao sistema para executar a leitura
-  mov EBX, 0      ; lê da entrada padrão
-  mov ECX, "\n"   ; quem deve receber o caractere lido
-  mov EDX, 1        ; tamanho da entrada
-  int 0x80          ; chamada do kernem do linux
+  le_enter_apos_primeiro_numero:    ; Este código lê e ignora o "\n" (enter) após a entrada do primeiro numero
+    mov EAX, 3      ; 3 indica ao sistema para executar a leitura
+    mov EBX, 0      ; lê da entrada padrão
+    mov ECX, enter1   ; quem deve receber o caractere lido
+    mov EDX, 1        ; tamanho da entrada
+    int 0x80          ; chamada do kernem do linux
 
-  mov EAX, 3      ; 3 indica ao sistema para executar a leitura
-  mov EBX, 0      ; lê da entrada padrão
-  mov ECX, vetor2   ; quem deve receber o caractere lido
-  mov EDX, 3        ; tamanho da entrada
-  int 0x80
+  le_vetor2:                        ; Este código lê os chars e armazena o segundo número de três dígitos no vetor2
+    mov EAX, 3      ; 3 indica ao sistema para executar a leitura
+    mov EBX, 0      ; lê da entrada padrão
+    mov ECX, vetor2   ; quem deve receber o caractere lido
+    mov EDX, 3        ; tamanho da entrada
+    int 0x80
 
-  ; esse codigo converte o numero 1 de vetor para inteiro
-  mov EDX, 100
-  mov ECX, 3
-  mov EBX, 0
-  convertendo_number1:
-      mov EAX, [vetor1 + EBX]     ; movendo conteudo de casa no vetor para EAX
-      sub EAX, 48      ; na tabela ASCII, '0' é 48 em decimal
-      imul EDX   ; multiplica a posição do vetor por EDX (100, 10, 1)
-      add EBX, 1
-      add [number1], EAX     ; salva o número na variável number
-      mov EAX, EDX        ; dividindo EDX para a próxima casa, move EDX para EAX para dividi-lo por 10
-      mov EDX, 10           ; salvando 10 no registrador EDX para jogar na variável [divisor]
-      mov [divisor], EDX    ; [divisor] = 10
-      mov EDX, 0          ; zera o edx para fazer a expansão
-      cdq                 ; dividendo EDX:EAX
-      idiv dword [divisor]        ; dividindo o EDX:EAX por 10 (divisor)
-      mov EDX, EAX        ; movendo o resultado da divisão para EDX
-  loop convertendo_number1
+  converte_vetor1_para_inteiro:     ; Este código converte o primeiro número do vetor para inteiro
+    mov EDX, 0
+    mov [number1], EDX
+    convertendo_number1:
+              mov AL, [vetor1 + 0]     ; movendo conteudo da primeira casa do vetor para AL
+              sub AL, 48      ; na tabela ASCII, '0' é 48 em decimal
+              mov DL, 100     ; movendo 100 para DL para multiplicação
+              imul DL        ;  AX = AL * DL
+              add [number1], AX    ; adicionando na variável number1 para transformar no número
+              mov AL, [vetor1 + 1] ; movendo conteudo da segunda casa do vetor para AL
+              sub AL, 48            ; na tabela ASCII, '0' é 48 em decimal
+              mov DL, 10            ; movendo 10 para DL para multiplicação
+              imul DL               ; AX = AL * DL
+              add [number1], AX
+              mov AL, [vetor1 + 2] ; movendo conteudo da terceira casa do vetor para AL
+              sub AL, 48        ; na tabela ASCII, '0' é 48 em decimal
+              add [number1], AL
 
-  ; esse codigo converte o numero 2 de vetor para inteiro
-  mov EDX, 100
-  mov ECX, 3
-  mov EBX, 0
-  convertendo_number2:
-      mov EAX, [vetor2 + EBX]    ; movendo conteudo de casa no vetor para EAX
-      sub EAX, 48      ; na tabela ASCII, '0' é 48 em decimal
-      imul EDX   ; multiplica a posição do vetor por EDX (100, 10, 1)
-      add EBX, 1
-      add [number2], EAX     ; salva o número na variável number
-      mov EAX, EDX        ; dividindo EDX para a próxima casa, move EDX para EAX para dividi-lo por 10
-      mov EDX, 10           ; salvando 10 no registrador EDX para jogar na variável [divisor]
-      mov [divisor], EDX    ; [divisor] = 10
-      mov EDX, 0          ; zera o edx para fazer a expansão
-      cdq                 ; dividendo EDX:EAX
-      idiv dword [divisor]        ; dividindo o EDX:EAX por 10 (divisor)
-      mov EDX, EAX        ; movendo o resultado da divisão para EDX
-  loop convertendo_number2
+  converte_vetor2_para_inteiro:     ; Este código converte o segundo número do vetor para inteiro
+       mov EDX, 0
+       mov [number2], EDX
+       convertendo_number2:
+              mov AL, [vetor2 + 0]     ; movendo conteudo da primeira casa do vetor para AL
+              sub AL, 48      ; na tabela ASCII, '0' é 48 em decimal
+              mov DL, 100     ; movendo 100 para DL para multiplicação
+              imul DL        ;  AX = AL * DL
+              add [number2], AX    ; adicionando na variável number1 para transformar no número
+              mov AL, [vetor2 + 1] ; movendo conteudo da segunda casa do vetor para AL
+              sub AL, 48            ; na tabela ASCII, '0' é 48 em decimal
+              mov DL, 10            ; movendo 10 para DL para multiplicação
+              imul DL               ; AX = AL * DL
+              add [number2], AX
+              mov AL, [vetor2 + 2] ; movendo conteudo da terceira casa do vetor para AL
+              sub AL, 48        ; na tabela ASCII, '0' é 48 em decimal
+              add [number2], AL
 
-  ; realizando a soma dos dois numeros
-  mov EAX, 0
-  add EAX, number1
-  add EAX, number2
+  soma_os_dois_numeros:             ; Este código realiza a soma dos dois números inteiros
+       mov EAX, 0            ; zerando AX
+       add AX, [number1]      ; AX = number1
+       add AX, [number2]      ; AX = number1 + number2
 
-  ;convertendo [resultado] em vetor
+  transforma_numero_em_vetor_para_impressao:    ; Este código pega o resultado final em inteiro e transforma em vetor para impressão
+      mov [resultado], AX
+      mov BX, 1000
+      mov DX, 0
+      cwd ; estender AX para DX:AX
+      idiv BX
+      cmp AX, 1
+      je resultado_em_4_casas
+      jne resultado_em_3_casas
+      resultado_em_4_casas:
+        add AX, 48
+        mov [vetor_print4], AX
+        sub AX, 48
+        mov AX, [resultado]
+        mov CX, 1000
+        sub AX, CX
+        mov BX, 100
+        mov DX, 0
+        idiv BX
+        add AX, 48
+        mov [vetor_print4 + 1], AX
+        sub AX, 48
+        mov DX, 100
+        imul AX, DX ; AX = AX * 100
+        add CX, AX
+        mov AX, [resultado]
+        sub AX, CX
+        mov BX, 10
+        mov DX, 0
+        idiv BX
+        add AX, 48
+        mov [vetor_print4 + 2], AX
+        sub AX, 48
+        mov DX, 10
+        imul AX, DX ; AX = AX * 10
+        add CX, AX
+        mov AX, [resultado]
+        sub AX, CX
+        add AX, 48
+        mov [vetor_print4 + 3], AX
+        jmp imprime_vetor_4_casas_com_interrupcao
 
+      resultado_em_3_casas:
+          mov CX, 0
+          mov AX, [resultado]
+          mov BX, 100
+          mov DX, 0
+          idiv BX
+          add AX, 48
+          mov [vetor_print3], AX
+          sub AX, 48
+          mov DX, 100
+          imul AX, DX ; AX = AX * 100
+          add CX, AX
+          mov AX, [resultado]
+          sub AX, CX
+          mov BX, 10
+          mov DX, 0
+          idiv BX
+          add AX, 48
+          mov [vetor_print3 + 1], AX
+          sub AX, 48
+          mov DX, 10
+          imul AX, DX ; AX = AX * 10
+          add CX, AX
+          mov AX, [resultado]
+          sub AX, CX
+          add AX, 48
+          mov [vetor_print3 + 2], AX
+          sub AX, 48
+          jmp imprime_vetor_3_casas_com_interrupcao
 
+  imprime_vetor_3_casas_com_interrupcao:        ; Este código imprime a soma (com 3 casas) transformado em vetor
+        mov eax, 4      ; sys_write()
+        mov ebx, 1      ; ... to STDOUT
+        mov ecx, vetor_print3 ; ... using the following memory address
+        mov edx, 3      ; ... and only print one character
+        int 80h         ; SYSCALL
+        jmp finalizar_programa
 
-  ;imprimindo o valor do resultado que está em EAX
-  ;mov EAX, 4
-  ;mov EBX
+  imprime_vetor_4_casas_com_interrupcao:        ; Este código imprime a soma (com 4 casas) transformado em vetor
+         mov eax, 4      ; sys_write()
+         mov ebx, 1      ; ... to STDOUT
+         mov ecx, vetor_print4 ; ... using the following memory address
+         mov edx, 4      ; ... and only print one character
+         int 80h         ; SYSCALL
+
+  finalizar_programa:   ; Este código finaliza o programa através do uso de interrupção
+      mov edx, 10
+      mov [enter1], edx
+      ; Imprimir quebra de linha
+      mov eax, 4      ; sys_write()
+      mov ebx, 1      ; ... to STDOUT
+      mov ecx, enter1 ; ... using the following memory address
+      mov edx, 1      ; ... and only print one character
+      int 80h         ; SYSCALL
+      ; System exit
+      mov eax,1
+      mov ebx,0
+      int 80h
